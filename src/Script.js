12082a3620d15ecb59171d4e7ca88d406fa009ad
aryanbhimani <aryan.bhimani.93@gmail.com>
2025-01-01 15:18:31 +0530

    first commit

---
+++ src/Script.js
@@ -0,0 +1,10 @@
+// Script.js
+
+// Function to display a greeting message
+function displayGreeting() {
+    const greeting = "Hello, welcome to MY App!";
+    console.log(greeting);
+}
+
+// Call the function to display the greeting
+displayGreeting();
